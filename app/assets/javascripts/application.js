45953377d1aec744a0518ecda91bbcf2dab315e6
Scott Magdalein <scottmagdalein@gmail.com>
2013-07-02 22:50:20 -0400

    aded parallax and sticky nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require stellar.js
+//= require jquery.smint.js
 //= require foundation
 //= require_tree .
 
---
+++ app/assets/javascripts/nav.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('.nav').smint({
+		'scrollSpeed': 1000
+	});
+});
